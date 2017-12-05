27fe4d14203c290c8a07829130ba0e571f69e7f1
Marcelo <marcelooferreira@outlook.com.br>
2017-12-05 10:40:11 -0200

    Estrutura básica do site.

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<html>
+	<head>
+		<title>Norte a Sul</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width">
+		<!--<script language="JavaScript" src="js/jquery.js"></script>-->
+		<link rel="stylesheet" type="text/css" href="css/mobile.css" media="all and (max-width: 480px)">
+		<link rel="stylesheet" type="text/css" href="css/desktop.css" media="all and (min-width: 481px)">
+	</head>
+    <body>
+        
+    </body>
+</html>
